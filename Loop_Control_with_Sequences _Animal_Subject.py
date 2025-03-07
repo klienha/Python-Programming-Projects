691f167703c77ca11d21f6bae9f064798a5e596d
klienha <106008442+klienha@users.noreply.github.com>
2025-03-07 13:37:50 -0500

    Loop_Control_with_Sequences _Animal_Subject.py

--- Loop_Control_with_Sequences _Animal_Subject.py
+++ Loop_Control_with_Sequences _Animal_Subject.py
@@ -1,10 +1,10 @@
-#helper function
+#Is animal given in a list a vertebrate.
 def is_vertebrate(name, vertebrates):
     for vert in vertebrates:
         if name==vert:
             return True
     return False
-
+#Given a list animals, a corresponding list weights (in lbs), and a list vertebrates, return the name of the animal that is the heaviest vertebrate. 
 def biggest_vertebrate(animals, weights, vertebrates):
     biggest = None
     biggest_i = None
@@ -18,7 +18,7 @@ def biggest_vertebrate(animals, weights, vertebrates):
     return None
 
 
- 
+#Create and return a list of the names of all animals that weigh no more than the given weight limit.
 def within_weight(animals, weights, weightLimit):
     meets = []
     for i in range(len(animals)):
@@ -26,7 +26,7 @@ def within_weight(animals, weights, weightLimit):
             meets.append(animals[i])
     return meets
     
-
+#Given a list of strings animals, and a list of strings vertebrates, return True if there are any vertebrates in the animals list directly next to each other, False if not.
 def any_adjacent_vertebrates(animals, vertebrates):
     for i in range(len(animals)-1):
         if is_vertebrate(animals[i],vertebrates) and is_vertebrate(animals[i+1],vertebrates):
@@ -34,7 +34,7 @@ def any_adjacent_vertebrates(animals, vertebrates):
     return False
     
 
-
+#Given a list of numbers weight_list, count how many ways unique pairs of values from this list have a sum equal to a value anywhere in the list (including equaling one of the values being added).
 def count_weights(weight_list):
     counter = 0
     for i in range(0, len(weight_list)):
